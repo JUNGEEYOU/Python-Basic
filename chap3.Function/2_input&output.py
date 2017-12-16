fb55bdc07c22df6a87333974c96bd59ebcf9a677
JUNGEEYOU <junge2u@naver.com>
2017-12-17 01:43:26 +0900

    파일 입력 출력

---
+++ chap3.Function/2_input&output.py
@@ -0,0 +1,13 @@
+#2. 사용자 입력과 출력 
+a =input()
+print(a)
+
+number = input('숫자를 입력하세요: ')
+
+# 출력하기 
+print("life" 'is' 'too short') #+와 동일 
+#- 띄어쓰기는 ,로 가능하다
+print("life",'is','too short')
+
+for i in range(10):
+    print(i,end=",")

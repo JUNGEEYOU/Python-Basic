8bd08f5a73b99939adddf2795c3d593ae780a871
JUNGEEYOU <junge2u@naver.com>
2017-12-17 15:37:35 +0900

    모듈

---
+++ chap4.Class&module/2_module.py
@@ -0,0 +1,14 @@
+#2. 모듈 : 함수나 변수 또는 클래스를 모아 놓은 파일 /다른 사람이 만들어 놓은 모듈 사용 or 모듈을 만들 수 있음!!
+# import mode1
+# print(mode1.safe_sum(1,'a'))
+
+
+#2) 모듈 이름 붙이지 않는 방법
+# from mode1 import sum
+
+# from mode1 import *
+# print(sum(1,2))
+# print(safe_sum(1,'a'))
+
+
+
